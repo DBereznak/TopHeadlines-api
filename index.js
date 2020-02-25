4efc360b33df0c955288c3132a23d93ea1797dc9
DonnyB <donny.bereznak@bluesombrero.com>
2020-02-25 12:11:11 -0500

    updated port variable

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require("express");
-require("dotenv").config();
-const routes = require("./routes/routes");
+const express = require('express');
+require('dotenv').config();
+const routes = require('./routes/routes');
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(routes);
 
-app.get("/", (req, res) => {
-  res.send("Server is on");
+app.get('/', (req, res) => {
+	res.send('Server is on');
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
